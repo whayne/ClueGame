7793d94eeb77fbfbf88c7a7b2593b3467327c6be
unknown <antnguye@CT156-27.adit.mines.edu>
2013-09-26 18:32:55 -0600

    IntBoard Barebones

---
+++ src/Clue/IntBoard.java
@@ -0,0 +1,46 @@
+package Clue;
+
+import java.util.*;
+
+public class IntBoard {
+
+	/**
+	 * @param args
+	 */
+	public IntBoard(){
+		
+	}
+	
+	public Map<Integer, LinkedList<Integer>> calcAdjacencies(){
+		Map<Integer, LinkedList<Integer>> adj = new HashMap<Integer, LinkedList<Integer>>();
+		return adj;
+	}
+	public void startTargets(int row, int col, int steps){
+		
+	}
+	
+	public void startTargets(int location, int steps){
+		
+	}
+	
+	public Set<Integer> getTargets() {
+		Set<Integer> targets = new HashSet<Integer>();
+		return targets;
+	}
+	
+	public LinkedList<Integer> getAdjList() {
+		LinkedList<Integer> adjList = new LinkedList<Integer>();
+		return adjList;
+	}
+	
+	public int calcIndex(int row, int col) {
+		return 1;
+	}
+	
+	
+	public static void main(String[] args) {
+		IntBoard board = new IntBoard();
+
+	}
+
+}
